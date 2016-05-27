8d5c23d2c0f45697fed9e91148e79eaa5cb97ea6
chaopan <chaopan0329@gmail.com>
2016-05-27 12:41:47 -0700

    spider movement improved, new spider box (maybe needs glass), notes added to the spider script for levels

--- Assets/RandomWalkspider.cs
+++ Assets/RandomWalkspider.cs
@@ -2,11 +2,17 @@
 using System.Collections;
 
 public class RandomWalkspider : MonoBehaviour {
-
-	// Use this for initialization
+    Animator anim;
+    // Use this for initialization
+    // TODO: make the spider only walk inside the box.
+    // NOTES: My idea of implementing the level progression:
+    //  Lvl 1: Just display the spider
+    //  Lvl 2: When player presses green button, show the 'ghost hand' asset and switch out the spider to a scripted spider that will crawl on the ghost hand once the player arm is in place
+    //  Lvl 3: Same as lvl 2, but a different spider that is scripted to jump onto the hand
+    //  Lvl 4: Multiple spiders.
 	void Start () {
-	
-	}
+        anim = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
     bool walking = false;
@@ -35,23 +41,26 @@ public class RandomWalkspider : MonoBehaviour {
         for (float i = 0; i < randomTime; i += Time.deltaTime)
         {
             yield return null;
+            anim.CrossFade("idle", 0f);
             print("inside pause");
             //TODO: make spider not do 'walk' animation
             
         }
         //turn
-        for (float i = 0; i < 2f; i += Time.deltaTime)
+        for (float i = 0; i < .5f; i += Time.deltaTime)
         {
             yield return null;
+            anim.CrossFade("walk", 0f);
             walking = true;
             Quaternion rotation = Quaternion.LookRotation(randomPos - originPos);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 2);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 4);
             //this.transform.rotation = Quaternion.Slerp(currentRotation, neededRotation, Time.deltaTime);
         }
         //walk
         for (float i = 0; i < (.3f * rand); i += Time.deltaTime)
         {
             yield return null;
+            anim.CrossFade("walk", 0f);
             //this.transform.LookAt(new Vector3(rand,this.transform.position.y,rand));
             //this.transform.LookAt(randomPos);
             //this.transform.rotation = Quaternion.Euler(this.transform.eulerAngles.x, randDir, transform.eulerAngles.z); 
